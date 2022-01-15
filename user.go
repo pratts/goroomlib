9330d6855e0b83676409b642afcae627fbe5d495
Prateek Sharma <prateeksharma.2801@gmail.com>
2022-01-15 23:36:19 +0530

    setting is connected flag

--- user.go
+++ user.go
@@ -6,6 +6,7 @@ type User struct {
 	name           string
 	joinedRooms    map[string]Room
 	userProperties map[string]interface{}
+	isConnected    bool
 }
 
 func (u *User) GetId() int {
@@ -18,6 +19,14 @@ func (u *User) GetUserId() int {
 
 func (u *User) GetName() string {
 	return u.name
+}
+
+func (u *User) SetIsConnected(isConnected bool) {
+	u.isConnected = isConnected
+}
+
+func (u *User) GetIsConnected() bool {
+	return u.isConnected
 }
 
 func (u *User) GetJoinedRooms() map[string]Room {
